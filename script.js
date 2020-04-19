4b9bccdb8429bb4ae643d2b252d2dfda77efd208
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-04-19 19:18:42 -0400

    local storage set

--- script.js
+++ script.js
@@ -17,9 +17,11 @@ $("#submit").on("click", function(){
         //console.log(response);
 
         weatherResults = response;
+        localStorage.setItem("weatherResults", JSON.stringify(weatherResults));
+        console.log(typeof localStorage.getItem("weatherResults"));
+    
         // calculate farenheit
-        var temp = parseInt((weatherResults.main.temp - 273.15) * 1.80 +32);
-        
+        var temp = parseInt((weatherResults.main.temp - 273.15) * 1.80 +32);        
         var date = new Date().toLocaleDateString();
         // weather icon code from openweather
         var icon = weatherResults.weather[0].icon;
@@ -54,7 +56,9 @@ function getUVIndex(lat, lon) {
         method: "GET"
     }).then(function(response){
         //console.log(response);
-        var uvValue = response.value;
+        var uvValue = response.value;        
+        localStorage.setItem("uvValue", uvValue);
+      
         $("#uv-index").text(uvValue);  
 
         if (uvValue <= 2) {
@@ -80,8 +84,11 @@ function getForeCast(cityStr) {
         method: "GET"
     }).then(function(response){
         //console.log(response);
-        var forecastList = response.list; 
-        console.log(forecastList);
+        var forecastList = response.list;
+        //console.log(forecastList);
+        localStorage.setItem("forecastList", JSON.stringify(forecastList));
+        console.log(typeof localStorage.getItem("forecastList"));
+        
         var j = 0;
         for (var i = 7; i < forecastList.length; i+=8) {
 
@@ -107,7 +114,17 @@ function getForeCast(cityStr) {
             }
         }
     });
-}    
+}
+
+function generateWeatherButtons() {
+
+    //localStorage.setItem("weatherResults", JSON.stringify(weatherResults));
+    //localStorage.setItem("uvValue", uvValue);
+
+    //localStorage.setItem("forecastList", JSON.stringify(forecastList));
+
+}
+
        
       
        
